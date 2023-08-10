9606273a0697b677438c75e07b57115f6565c912
Lady Soto <lady.sotodavila@gmail.com>
2023-08-09 21:40:10 -0500

    Se crean clases de implementación y testeo

--- Alzate-Leidy_Soto-Lady/src/com/backend/integrador/Aplicacion.java
+++ Alzate-Leidy_Soto-Lady/src/com/backend/integrador/Aplicacion.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 
 public class Aplicacion {
     public static void main(String[] args) {
+
         Connection connection = null;
         try{
             Class.forName("org.h2.Driver");
             connection = DriverManager.getConnection("jdbc:h2:~parcialbackend;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
-            System.out.println("conexion creada");
         } catch (Exception e){
             e.printStackTrace();
         } finally {
