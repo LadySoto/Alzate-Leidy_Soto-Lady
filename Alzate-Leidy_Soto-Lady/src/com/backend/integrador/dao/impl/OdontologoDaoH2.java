31174b2c17759d09ce0e7460a47e9ec67384a6c2
Lady Soto <lady.sotodavila@gmail.com>
2023-08-09 22:11:53 -0500

    Agregados los métodos de la interfaz

--- Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
+++ Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
@@ -3,5 +3,16 @@ package com.backend.integrador.dao.impl;
 import com.backend.integrador.dao.IDao;
 import com.backend.integrador.entity.Odontologo;
 
+import java.util.List;
+
 public class OdontologoDaoH2 implements IDao<Odontologo> {
+    @Override
+    public Odontologo registrar(Odontologo odontologo) {
+        return null;
+    }
+
+    @Override
+    public List<Odontologo> listarOdontologos() {
+        return null;
+    }
 }
