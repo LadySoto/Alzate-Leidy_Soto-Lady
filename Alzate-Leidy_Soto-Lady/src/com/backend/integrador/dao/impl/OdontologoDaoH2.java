a5ed438763ebea3a3442dcc4f5afd387389f38b9
ljac <leidy-j25@hotmail.com>
2023-08-09 23:02:34 -0500

    Avance examen

--- Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
+++ Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
@@ -1,18 +1,99 @@
 package com.backend.integrador.dao.impl;
 
+import com.backend.integrador.dao.H2Connection;
 import com.backend.integrador.dao.IDao;
 import com.backend.integrador.entity.Odontologo;
+import org.apache.log4j.Logger;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoH2.class);
     @Override
     public Odontologo registrar(Odontologo odontologo) {
-        return null;
+        Connection connection = null;
+        String insert = "INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO) VALUES (?,?,?)";
+        Odontologo odontologo1 = null;
+
+        try {
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+            PreparedStatement pst = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            pst.setInt(1, odontologo.getMatricula());
+            pst.setString(2, odontologo.getNombre());
+            pst.setString(3, odontologo.getApellido());
+            pst.execute();
+
+            connection.commit();
+            odontologo1 = new Odontologo(odontologo.getMatricula(), odontologo.getNombre(), odontologo.getApellido());
+            ResultSet key = pst.getGeneratedKeys();
+            while (key.next()) {
+                odontologo1.setId(key.getInt(1));
+            }
+            LOGGER.info("Odontologo registrado: " + odontologo1);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    System.out.println("Tuvimos un problema");
+                    e.printStackTrace();
+                } catch (SQLException exception) {
+                    LOGGER.error(exception.getMessage());
+                    exception.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                LOGGER.error("No se pudo cerrar la conexion: " + ex.getMessage());
+            }
+        }
+        return odontologo1;
     }
+
 
     @Override
     public List<Odontologo> listarOdontologos() {
-        return null;
+        Connection connection = null;
+        Odontologo odontologo = null;
+        List<Odontologo> odontologosLista = null;
+        String select = "SELECT * FROM ODONTOLOGOS";
+
+        try {
+            connection = H2Connection.getConnection();
+            PreparedStatement pst = connection.prepareStatement(select);
+            ResultSet rst = pst.executeQuery();
+
+            while (rst.next()) {
+                int id = rst.getInt(1);
+                int matricula = rst.getInt(2);
+                String nombre = rst.getString(3);
+                String apellido = rst.getString(4);
+
+                odontologo = new Odontologo(id, matricula, nombre, apellido);
+                odontologosLista = new ArrayList<>();
+                odontologosLista.add(odontologo);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                LOGGER.error("No se pudo cerrar la conexion: " + ex.getMessage());
+            }
+
+            return odontologosLista;
+        }
     }
 }
+
+
--- Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoMemoria.java
+++ Alzate-Leidy_Soto-Lady/src/com/backend/integrador/dao/impl/OdontologoDaoMemoria.java
@@ -2,17 +2,36 @@ package com.backend.integrador.dao.impl;
 
 import com.backend.integrador.dao.IDao;
 import com.backend.integrador.entity.Odontologo;
+import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class OdontologoDaoMemoria implements IDao<Odontologo>{
+public class OdontologoDaoMemoria implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoMemoria.class);
+
+    private List<Odontologo> odontologoLista;
+
+    public OdontologoDaoMemoria(List<Odontologo> odontologoRepository) {
+        this.odontologoLista = odontologoRepository;
+    }
+
     @Override
     public Odontologo registrar(Odontologo odontologo) {
-        return null;
+        odontologoLista.add(odontologo);
+        LOGGER.info("Odontologo registrado: " + odontologo);
+        return odontologo;
     }
 
     @Override
     public List<Odontologo> listarOdontologos() {
-        return null;
+        for (Odontologo odontologo : odontologoLista) {
+            LOGGER.info("Este es el odontologo: " + odontologo);
+        }
+        return odontologoLista;
+
     }
 }
+
+
